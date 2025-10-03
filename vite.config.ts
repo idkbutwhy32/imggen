a861297b31fe9f57529312c9103ec36648afdf22
idkbutwhy32 <idkbutwhy32@gmail.com>
2025-10-03 16:43:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/YOUR_REPO_NAME/', // <-- ADD THIS LINE
+  base: '/imggen/', // <-- ADD THIS LINE
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
